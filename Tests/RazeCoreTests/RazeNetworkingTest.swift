fbad91498f794fc05962d57f4726eaab3f274f30
Sergio Vladimir Flores Zarco <svladimir@ia.com.mx>
2020-12-06 12:36:46 -0600

    Add second Networking API for sending data

--- Tests/RazeCoreTests/RazeNetworkingTest.swift
+++ Tests/RazeCoreTests/RazeNetworkingTest.swift
@@ -16,6 +16,16 @@ class NetworkSessionMock: NetworkSession {
     func get(from url: URL, completionHandler: @escaping (Data?, Error?) -> Void) {
         completionHandler(data, error)
     }
+
+    func post(with request: URLRequest, completionHandler: @escaping (Data?, Error?) -> Void) {
+        completionHandler(data, error)
+    }
+}
+
+struct MockData: Codable, Equatable {
+
+    var id: Int
+    var name: String
 }
 
 final class RazeNetworkingTest: XCTestCase {
@@ -37,6 +47,30 @@ final class RazeNetworkingTest: XCTestCase {
                 XCTFail(error?.localizedDescription ?? "Error forming error result")
             }
         }
+        wait(for: [expectation], timeout: 5)
+    }
+
+    func testSendDataCall() {
+        let session = NetworkSessionMock()
+        let manager = RazeCore.Networking.Manager()
+        let sampleObject = MockData(id: 1, name: "John")
+        let data = try? JSONEncoder().encode(sampleObject)
+        session.data = data
+        manager.session = session
+        let url = URL(fileURLWithPath: "url")
+        let expectation = XCTestExpectation(description: "Send data")
+
+        manager.sendData(to: url, body: sampleObject) { result in
+            expectation.fulfill()
+            switch result {
+            case .success(let returnedData):
+                let returnedObject = try? JSONDecoder().decode(MockData.self, from: returnedData)
+                XCTAssertEqual(returnedObject, sampleObject)
+            case .failure(let error):
+                XCTFail(error?.localizedDescription ?? "Error forming error reuslt")
+            }
+        }
+
         wait(for: [expectation], timeout: 5)
     }
 
